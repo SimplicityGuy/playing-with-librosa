df27c9b2489fe381894a21e830db2be24d0dfcfc
Robert Wlodarczyk <robert@simplicityguy.com>
2020-05-25 18:44:04 -0700

    Fixing comment and ignoring warnings from imported packages.

--- beat_detection.py
+++ beat_detection.py
@@ -1,7 +1,11 @@
 #! /usr/bin/env python3
 """Beat detection using librosa."""
 import argparse
-import librosa
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import librosa  # noqa: E402
 
 hop_length = 1024
 
@@ -13,15 +17,18 @@ def beat_detection(filename):
     y, sr = librosa.load(filename, sr=44100)
 
     # Beat track using the entire signal. Discard the beat frames.
-    tempo, _ = librosa.beat.beat_track(y=y, sr=sr, hop_length=hop_length)
-    print(f"Estimated tempo: {tempo:.2f} beats per minute.")
+    tempo1, _ = librosa.beat.beat_track(y=y, sr=sr, hop_length=hop_length)
+    print(f"Estimated bpm (using entire signal): {tempo1:.1f} beats per minute.")
 
     # Separate harmonics and percussives into two waveforms.
     _, y_percussive = librosa.effects.hpss(y)
 
     # Beat track on the percussive signal. Discard the beat frames.
-    tempo, _ = librosa.beat.beat_track(y=y_percussive, sr=sr)
-    print(f"Estimated tempo (using percussive signal): {tempo:.2f} beats per minute.")
+    tempo2, _ = librosa.beat.beat_track(y=y_percussive, sr=sr)
+    print(f"Estimated bpm (using percussive signal): {tempo2:.1f} beats per minute.")
+
+    tempo = round((tempo1 + tempo2) / 2)
+    print(f"Average bpm: {tempo}")
 
 
 def main():
