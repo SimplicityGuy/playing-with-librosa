6a0f1b75aa9c95af1408a3aa4de38d591002a3a4
Robert Wlodarczyk <robert@simplicityguy.com>
2020-05-27 18:21:51 -0700

    Mel spectrograph by source.

--- mel_spectrograph.py
+++ mel_spectrograph.py
@@ -13,7 +13,7 @@ import librosa.display  # noqa: E402
 hop_length = 1024
 
 
-def beat_detection(filename):
+def mel_spectrograph(filename):
     """Mel spectrograph using librosa."""
     # y  = loaded audio as waveform
     # sr = sampling rate
@@ -37,6 +37,35 @@ def beat_detection(filename):
     plt.show()
 
 
+def mel_spectrograph_by_source(filename):
+    """Mel spectrograph by source using librosa."""
+    # y  = loaded audio as waveform
+    # sr = sampling rate
+    y, sr = librosa.load(filename, sr=44100)
+    y_harmonic, y_percussive = librosa.effects.hpss(y)
+
+    # Mel-scaled power (energy-squared) spectrogram.
+    S_harmonic = librosa.feature.melspectrogram(y_harmonic, sr=sr)
+    S_percussive = librosa.feature.melspectrogram(y_percussive, sr=sr)
+
+    # Convert to log scale (dB). Use the peak power (max) as reference.
+    log_Sh = librosa.power_to_db(S_harmonic, ref=np.max)
+    log_Sp = librosa.power_to_db(S_percussive, ref=np.max)
+
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(2, 1, 1)
+    librosa.display.specshow(log_Sh, sr=sr, x_axis="time", y_axis="mel")
+    plt.title("mel power spectrogram (Harmonic)")
+    plt.colorbar(format="%+02.0f dB")
+    plt.subplot(2, 1, 2)
+    librosa.display.specshow(log_Sp, sr=sr, x_axis="time", y_axis="mel")
+    plt.title("mel power spectrogram (Percussive)")
+    plt.colorbar(format="%+02.0f dB")
+    plt.tight_layout()
+    plt.show()
+
+
 def main():
     """Parameter handling main method."""
     parser = argparse.ArgumentParser(description="Mel spectrograph using librosa.")
@@ -45,7 +74,8 @@ def main():
     filename = args["filename"]
     if filename is not None:
         print(f"Filename: {filename}")
-        beat_detection(filename)
+        mel_spectrograph(filename)
+        mel_spectrograph_by_source(filename)
 
 
 if __name__ == "__main__":
