67740a6a316766d5e3895cdacee626b8c3f890aa
Robert Wlodarczyk <robert@simplicityguy.com>
2020-05-27 19:04:05 -0700

    Expanding mel spectograph.

--- mel_spectrograph.py
+++ mel_spectrograph.py
@@ -13,14 +13,10 @@ import librosa.display  # noqa: E402
 hop_length = 1024
 
 
-def mel_spectrograph(filename):
+def mel_spectrograph(y, sr):
     """Mel spectrograph using librosa."""
-    # y  = loaded audio as waveform
-    # sr = sampling rate
-    y, sr = librosa.load(filename, sr=44100)
-
     # Mel-scaled power (energy-squared) spectrogram.
-    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+    S = librosa.feature.melspectrogram(y, sr=sr)
 
     # Convert to log scale (dB). Use the peak power (max) as reference.
     log_S = librosa.power_to_db(S, ref=np.max)
@@ -37,11 +33,8 @@ def mel_spectrograph(filename):
     plt.show()
 
 
-def mel_spectrograph_by_source(filename):
+def mel_spectrograph_by_source(y, sr):
     """Mel spectrograph by source using librosa."""
-    # y  = loaded audio as waveform
-    # sr = sampling rate
-    y, sr = librosa.load(filename, sr=44100)
     y_harmonic, y_percussive = librosa.effects.hpss(y)
 
     # Mel-scaled power (energy-squared) spectrogram.
@@ -65,17 +58,29 @@ def mel_spectrograph_by_source(filename):
     plt.tight_layout()
     plt.show()
 
+    return y_harmonic, y_percussive
+
 
 def main():
     """Parameter handling main method."""
     parser = argparse.ArgumentParser(description="Mel spectrograph using librosa.")
     parser.add_argument("-f", "--filename", type=str, help="mp3 or wav file to analyze")
+    parser.add_argument("--full_mel", action="store_true", help="full mel spectrograph")
+    parser.add_argument("--split_mel", action="store_true", help="per source mel spectrograph")
     args = vars(parser.parse_args())
+
     filename = args["filename"]
     if filename is not None:
         print(f"Filename: {filename}")
-        mel_spectrograph(filename)
-        mel_spectrograph_by_source(filename)
+        # y  = loaded audio as waveform
+        # sr = sampling rate
+        y, sr = librosa.load(filename, sr=44100)
+
+    if args.get("full_mel", False):
+        mel_spectrograph(y, sr)
+
+    if args.get("split_mel", False):
+        _, _ = mel_spectrograph_by_source(y, sr)
 
 
 if __name__ == "__main__":
